0fbfbe8518073689dbc1a93f6c438ca22e9ca08f
Danila Legkobytov <bjorn-bis@yandex.ru>
2022-11-03 21:59:20 +0700

    Hamburger

---
+++ blocks/hamburger/hamburger.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+  menuItem = document.querySelectorAll('.menu__item'),
+  hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+  });
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      })
+  })
+})
